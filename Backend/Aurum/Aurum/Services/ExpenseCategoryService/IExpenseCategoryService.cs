65d9de3e482882c55c49e545bf8fa1845c747e4b
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-01-21 19:14:51 +0100

    ExpenseCategoryService async methods created

--- Backend/Aurum/Aurum/Services/ExpenseCategoryService/IExpenseCategoryService.cs
+++ Backend/Aurum/Aurum/Services/ExpenseCategoryService/IExpenseCategoryService.cs
@@ -1,6 +1,9 @@
+using Aurum.Models.CategoryDto;
+
 namespace Aurum.Services.ExpenseCategoryService;
 
 public interface IExpenseCategoryService
 {
-	
+	Task<Dictionary<CategoryDto, List<SubCategoryDto>>> GetAllExpenseCategories(int userId);
+	Task<int> AcquireSubCategoryId(int categoryId, string subCategoryName);
 }
