3a5ca481acd29c2edc1c6ba7b57fc655d8486d31
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-01-21 20:05:26 +0100

    ExpenseService interface argument name refactor

--- Backend/Aurum/Aurum/Services/ExpenseService/IExpenseService.cs
+++ Backend/Aurum/Aurum/Services/ExpenseService/IExpenseService.cs
@@ -5,7 +5,7 @@ namespace Aurum.Services.ExpenseService;
 
 public interface IExpenseService
 {
-	Task<List<ExpenseDto>> GetAll(int accountId, int userID);
-	Task<List<ExpenseDto>> GetAll(int accountId, int userID, DateTime startDate, DateTime endDate);
+	Task<List<ExpenseDto>> GetAll(int accountId, int userId);
+	Task<List<ExpenseDto>> GetAll(int accountId, int userId, DateTime startDate, DateTime endDate);
 	Task<int> Create(ModifyExpenseDto expense);
 }
