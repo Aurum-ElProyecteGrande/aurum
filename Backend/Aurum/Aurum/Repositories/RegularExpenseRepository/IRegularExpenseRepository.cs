ed35cb8b91d235235dea019c4d19268fa750792e
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-01-21 22:45:40 +0100

    Refactored ExpenseRepository interface

--- Backend/Aurum/Aurum/Repositories/RegularExpenseRepository/IRegularExpenseRepository.cs
+++ Backend/Aurum/Aurum/Repositories/RegularExpenseRepository/IRegularExpenseRepository.cs
@@ -5,7 +5,7 @@ namespace Aurum.Repositories.RegularExpenseRepository;
 public interface IRegularExpenseRepository
 {
 	Task<List<RawRegularExpenseDto>> GetAllRegular(int accountId);
-	Task<int> Create(RegularExpenseDto expense);
-	Task<int> Update(RegularExpenseDto expense);
+	Task<int> Create(RawRegularExpenseDto expense);
+	Task<int> Update(RawRegularExpenseDto expense);
 	Task<bool> Delete(int regularId);
 }
