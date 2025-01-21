e496767209e9c016d44bcb2c66af81832f6138ac
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-01-21 20:27:58 +0100

    income category controller implemented

---
+++ Backend/Aurum/Aurum/Controllers/IncomeCategoryController/IncomeCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Aurum.Models.CategoryDTOs;
+using Aurum.Repositories.Income;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aurum.Controllers.Categories
+{
+    [ApiController]
+    public class IncomeCategoryController : ControllerBase
+    {
+        private IIncomeCategoryRepo _incomeCategoryRepo;
+        private ILogger<IncomeCategoryController> _logger;
+
+        public IncomeCategoryController(IIncomeCategoryRepo incomeCategoryRepo, ILogger<IncomeCategoryController> logger)
+        {
+            _incomeCategoryRepo = incomeCategoryRepo;
+            _logger = logger;
+        }
+
+        [HttpGet("/categories/expense")]
+        public ActionResult<List<CategoryDto>> GetAll()
+        {
+            try
+            {
+                var incomeCategories = _incomeCategoryRepo.GetAllCategory();
+
+                return Ok(incomeCategories);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
