5f8e173507b83b163ff553327b30b15d4038fb4b
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-01-22 12:09:39 +0100

    expense service & repo updated

--- Backend/Aurum/Aurum/Services/ExpenseService/IExpenseService.cs
+++ Backend/Aurum/Aurum/Services/ExpenseService/IExpenseService.cs
@@ -9,4 +9,6 @@ public interface IExpenseService
 	Task<List<ExpenseDto>> GetAll(int accountId, int userId, DateTime startDate, DateTime endDate);
 	Task<int> Create(ModifyExpenseDto expense);
 	Task<bool> Delete(int expenseId);
+	Task<decimal> GetTotalExpense(int accountId);
+	Task<decimal> GetTotalExpense(int accountId, DateTime date);
 }
