afbf02d17449e34cc2e4e0450cafcfc137a6350b
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-01-22 12:09:07 +0100

    account service added

---
+++ Backend/Aurum/Aurum/Services/AccountService/AccountService.cs
@@ -0,0 +1,10 @@
+﻿namespace Aurum.Services.AccountService
+{
+    public class AccountService : IAccountService
+    {
+        public async Task<decimal> GetInitialAmount(int accountId)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
---
+++ Backend/Aurum/Aurum/Services/AccountService/IAccountService.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurum.Services.AccountService
+{
+    public interface IAccountService
+    {
+        Task<decimal> GetInitialAmount(int accountId);
+    }
+}
