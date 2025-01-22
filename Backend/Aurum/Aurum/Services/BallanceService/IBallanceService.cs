7ddb0a4c9c15ce5367f23e874f2de753ac7ea8d3
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-01-22 12:10:23 +0100

    ballance service & controller added

---
+++ Backend/Aurum/Aurum/Services/BallanceService/IBallanceService.cs
@@ -0,0 +1,10 @@
+﻿namespace Aurum.Services.BallanceService
+{
+    public interface IBallanceService
+    {
+        DateTime ValidateDate(DateTime? date);
+
+        Task<decimal> GetBallance(int accountId);
+        Task<decimal> GetBallance(int accountId, DateTime date);
+    }
+}
