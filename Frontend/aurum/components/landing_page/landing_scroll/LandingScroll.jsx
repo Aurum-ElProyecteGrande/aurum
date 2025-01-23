cefb368f1671edf46b6b06e6d0030f9fd91ee292
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-01-23 19:32:42 +0100

    Added landing scroll section

---
+++ Frontend/aurum/components/landing_page/landing_scroll/LandingScroll.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
+import { scrollInfo } from '@/scripts/landing_page_scripts/landing_page.js'
+
+const LandingScroll = () => {
+    return (
+        <section className="landing-scroll">
+            <div className="landing-scroll-container wrapper">
+                {
+                    scrollInfo.map(({ id, img, alt }) => (
+                        <Image
+                            key={id}
+                            src={img}
+                            alt={alt}
+                            width={500}
+                            height={500}
+                        />
+                    ))
+                }
+                {
+                    scrollInfo.reverse().map(({ id, img, alt }) => (
+                        <Image
+                            key={id}
+                            src={img}
+                            alt={alt}
+                            width={500}
+                            height={500}
+                        />
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+export default LandingScroll
