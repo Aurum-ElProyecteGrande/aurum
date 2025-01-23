8033c96ac9b7b9600f2ba9d49fd5239bb7cd76c7
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-01-23 16:32:05 +0100

    Added expense repositories and services as scoped service

--- Backend/Aurum/Aurum/Program.cs
+++ Backend/Aurum/Aurum/Program.cs
@@ -4,14 +4,19 @@ using Aurum.Repositories.Income.RegularIncome;
 using Aurum.Models.CustomJsonConverter;
 using Aurum.Models.RegularExpenseDto;
 using Aurum.Models.RegularityEnum;
+using Aurum.Repositories.ExpenseCategoryRepository;
+using Aurum.Repositories.ExpenseRepository;
+using Aurum.Repositories.RegularExpenseRepository;
 using Aurum.Services.AccountService;
-using Aurum.Services.BallanceService;
+using Aurum.Services.BalanceService;
+using Aurum.Services.ExpenseCategoryService;
+using Aurum.Services.ExpenseService;
 using Aurum.Services.Income;
+using Aurum.Services.RegularExpenseService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers()
 	.AddJsonOptions(options => 
 	{ 
@@ -24,9 +29,26 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IIncomeRepo, IncomeRepo>();
 builder.Services.AddScoped<IRegularIncomeRepo, RegularIncomeRepo>();
 builder.Services.AddScoped<IIncomeCategoryRepo, IncomeCategoryRepo>();
+builder.Services.AddScoped<IExpenseCategoryRepository, ExpenseCategoryRepository>();
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IRegularExpenseRepository, RegularExpenseRepository>();
 builder.Services.AddScoped<IIncomeService, IncomeService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IExpenseCategoryService, ExpenseCategoryService>();
+builder.Services.AddScoped<IExpenseService, ExpenseService>();
+builder.Services.AddScoped<IRegularExpenseService, RegularExpenseService>();
 builder.Services.AddScoped<IBalanceService, BalanceService>();
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowFrontEnd",
+		policy =>
+		{
+			policy.WithOrigins("http://localhost:3000")
+				.AllowAnyHeader()
+				.AllowAnyMethod();
+		});
+});
 
 var app = builder.Build();
 
@@ -36,6 +58,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+app.UseCors("AllowFrontEnd");
 
 app.UseHttpsRedirection();
 
