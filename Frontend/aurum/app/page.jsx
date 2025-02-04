9449183d29ef1fbae4e9b0564de9684fae5ce217
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-02-04 20:40:14 +0100

    Base changes

--- Frontend/aurum/app/page.jsx
+++ Frontend/aurum/app/page.jsx
@@ -1,17 +1,28 @@
+"use client"
+import { useEffect } from "react";
 import LandingNavbar from "@/components/landing_page/landing_nav/LandingNavbar";
 import LandingHero from "@/components/landing_page/landing_hero/LandingHero";
 import LandingScroll from "@/components/landing_page/landing_scroll/LandingScroll";
 import LandingPrices from "@/components/landing_page/landing-prices/LandingPrices";
 import LandingNewsletter from "@/components/landing_page/landing_newsletter/LandingNewsLetter";
 import LandingFooter from "@/components/landing_page/landing_footer/LandingFooter";
+import LandingShowcase from "@/components/landing_page/landig_showcase/LandingShowcase";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
+
+  useEffect(() => {
+    Aos.init({ duration: 750 });
+  }, []);
+
   return (
     <>
       <LandingNavbar />
       <LandingHero />
       <LandingScroll />
       <LandingPrices />
+      <LandingShowcase />
       <LandingNewsletter />
       <LandingFooter />
     </>
