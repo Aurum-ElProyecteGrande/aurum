bd6feb49979c96e809b5d5ebf481bd340853593b
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-02-04 21:59:04 +0100

    layout refactor

--- Frontend/aurum/app/dashboard/page.jsx
+++ Frontend/aurum/app/dashboard/page.jsx
@@ -5,7 +5,8 @@ import { useState, useEffect } from "react";
 import HamburgerMenu from "../components/dashboard/hamburger-menu";
 import Header from "../components/dashboard/header";
 import ChangeChartForm from "../components/dashboard/change-chart-form";
-import { layouts } from "../../scripts/dashboard_scripts/dashboard_scripts"
+import Sidebar from "../components/sidebar";
+import { layouts } from "../../scripts/dashboard_scripts/layouts"
 
 export default function DashboardPage() {
 
@@ -24,13 +25,14 @@ export default function DashboardPage() {
   return (
     <div className="dashboard page">
       <Header setIsHamburgerOpen={setIsHamburgerOpen} isHamburgerOpen={isHamburgerOpen} isEditMode={isEditMode} />
+      <Sidebar />
       {isHamburgerOpen &&
         <HamburgerMenu isEditMode={isEditMode} setIsEditMode={setIsEditMode} choosenLayout={choosenLayout} setChoosenLayout={setChoosenLayout} />
       }
       <div className="dashboard-container">
         {possibleChartsBySegment.map((possibleCharts, segmentIndex) => (
-          <div key={`${possibleCharts}-${segmentIndex}`} className={`${choosenLayout}-${segmentIndex + 1} chart-container ${isEditMode && "edit-mode"}`}>
-            {choosenCharts[segmentIndex].chart}
+          <div key={segmentIndex} className={`${choosenLayout}-${segmentIndex + 1} chart-container ${isEditMode && "edit-mode"}`}>
+            {React.cloneElement(choosenCharts[segmentIndex].chart, { isEditMode })}
             {isEditMode &&
               <ChangeChartForm choosenCharts={choosenCharts} segmentIndex={segmentIndex} possibleCharts={possibleCharts} setChoosenCharts={setChoosenCharts} />
             }
