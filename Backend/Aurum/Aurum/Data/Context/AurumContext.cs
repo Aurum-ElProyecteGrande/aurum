9f833207c60f27e4f39792a2a43e5697240760f2
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-02-18 14:07:19 +0100

    currency beta

--- Backend/Aurum/Aurum/Data/Context/AurumContext.cs
+++ Backend/Aurum/Aurum/Data/Context/AurumContext.cs
@@ -1,4 +1,5 @@
 using Aurum.Data.Entities;
+using Aurum.Data.Seeders.DataReaders;
 using Aurum.Models.ExpenseDto;
 using Aurum.Models.RegularExpenseDto;
 using Microsoft.AspNetCore.Identity;
@@ -7,18 +8,36 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Aurum.Data.Context;
 
-public class AurumContext(DbContextOptions<AurumContext> options):IdentityDbContext<IdentityUser, IdentityRole, string>(options)
+public class AurumContext(DbContextOptions<AurumContext> options) : IdentityDbContext<IdentityUser, IdentityRole, string>(options)
 {
-	public DbSet<Account> Accounts { get; set; }
-	public DbSet<Currency> Currencies { get; set; }
-	public DbSet<Expense> Expenses { get; set; }
-	public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
-	public DbSet<ExpenseSubCategory> ExpenseSubCategories { get; set; }
-	public DbSet<Income> Incomes { get; set; }
-	public DbSet<IncomeCategory> IncomeCategories { get; set; }
-	public DbSet<RegularExpense> RegularExpenses { get; set; }
-	public DbSet<RegularIncome> RegularIncomes { get; set; }
-	public DbSet<BasicLayout> BasicLayouts{ get; set; }
-	public DbSet<ScienticLayout> ScienticLayouts{ get; set; }
-	public DbSet<DetailedLayout> DetailedLayouts { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Currency> Currencies { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+    public DbSet<ExpenseSubCategory> ExpenseSubCategories { get; set; }
+    public DbSet<Income> Incomes { get; set; }
+    public DbSet<IncomeCategory> IncomeCategories { get; set; }
+    public DbSet<RegularExpense> RegularExpenses { get; set; }
+    public DbSet<RegularIncome> RegularIncomes { get; set; }
+    public DbSet<BasicLayout> BasicLayouts { get; set; }
+    public DbSet<ScienticLayout> ScienticLayouts { get; set; }
+    public DbSet<DetailedLayout> DetailedLayouts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        CsvDataReader<Currency> _currencyReader = new CurrencyReader("currencies.csv");
+
+        // Seed data 
+
+        modelBuilder.Entity<IdentityUser>().HasData(
+        //CreateUsers() -> need pw hasher first
+        );
+
+        modelBuilder.Entity<Currency>().HasData(
+            _currencyReader.Read()
+            );
+
+    }
 }
---
+++ Backend/Aurum/Aurum/Data/Seeders/DataReaders/CurrencyReader.cs
@@ -0,0 +1,26 @@
+﻿using Aurum.Data.Entities;
+using Aurum.Repositories.CurrencyRepository;
+
+namespace Aurum.Data.Seeders.DataReaders
+{
+    public class CurrencyReader : CsvDataReader<Currency>
+    { 
+        public CurrencyReader(string fileName) : base(fileName) { }
+
+        public override List<Currency> Read()
+        { 
+            List<Currency> currencies = new();
+
+            using (_reader)
+            {
+                string line;
+                while ((line = _reader.ReadLine()) != null)
+                {
+                    var data = line.Split(",");
+                    currencies.Add(new Currency() { Name = data[0], CurrencyCode = data[1], Symbol = data[2] });
+                }
+            }
+            return currencies;
+        }
+    }
+}
