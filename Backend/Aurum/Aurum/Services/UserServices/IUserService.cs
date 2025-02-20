59b1f6f1dd1bbddbbb4165a32f6408bddafbf600
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-02-20 14:12:46 +0100

    refactored

--- Backend/Aurum/Aurum/Services/UserServices/IUserService.cs
+++ Backend/Aurum/Aurum/Services/UserServices/IUserService.cs
@@ -6,7 +6,8 @@ namespace Aurum.Services.UserServices
 {
     public interface IUserService
     {
-        Task<bool> Update(User user);
+        Task<AuthResponse> GetUserInfo(string userId);
+        Task<bool> Update(AuthResponse user, string userId);
         Task<bool> Delete(string userId);
         Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
         Task<AuthResult> LoginAsync(string email, string password);
