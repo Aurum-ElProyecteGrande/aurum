570ce831f8f7e9d72bb02a96570656b44224deee
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-03 13:39:59 +0100

    Folder restructure

---
+++ Backend/Aurum/Migrations/20250303122610_NewFolder.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Aurum.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFolder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "CurrencyId",
+                keyValue: -4);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "CurrencyId",
+                keyValue: -3);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "CurrencyId",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "CurrencyId",
+                keyValue: -1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "CurrencyId", "CurrencyCode", "Name", "Symbol" },
+                values: new object[,]
+                {
+                    { -4, "GBP", "Font", "P" },
+                    { -3, "USD", "US Dollar", "$" },
+                    { -2, "EUR", "Euro", "€" },
+                    { -1, "HUF", "Forint", "Ft" }
+                });
+        }
+    }
+}
