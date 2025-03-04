925eeb86cfc9c02e279b109677e411cf6f1abb0f
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-04 18:58:33 +0100

    auth fix refactor

--- Frontend/middleware.js
+++ Frontend/middleware.js
@@ -5,20 +5,7 @@ export async function middleware(request) {
 	
 	if (!token) 
 		return NextResponse.redirect(new URL("/", request.nextUrl));
-
-	/* const apiBaseUrl = "http://localhost:8080/user/validate";
-	const apiUrl = `${request.nextUrl.origin}/api/User/validate`;
-
-	const response = await fetch(new URL(apiBaseUrl), {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${token}`
-		},
-	});
-
-	if (!response.ok) 
-		return NextResponse.redirect(new URL("/", request.nextUrl)); */
-
+	
 	return NextResponse.next();
 }
 
