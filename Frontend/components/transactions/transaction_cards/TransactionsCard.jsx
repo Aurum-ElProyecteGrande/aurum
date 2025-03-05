88240fdfeadf7eb9ea72bf71f17566331097ce78
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-05 22:54:21 +0100

    mobile transactions added

---
+++ Frontend/components/transactions/transaction_cards/TransactionsCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { displayCurrency } from '@/scripts/dashboard_scripts/dashboard_scripts';
+
+const TransactionCards = ({ data }) => {
+    return (
+        <div className="transactions-cards">
+            {data.map((item, index) => (
+                <div className="transactions-card" key={index}>
+                    <div className="transactions-card-header">
+                        <h3>{item.label}</h3>
+                        <span>
+                            {new Date(item.date).toLocaleDateString()}
+                        </span>
+                    </div>
+                    <div className="transactions-card-body">
+                        <p>
+                            <strong>Category:</strong> {item.category.name}
+                        </p>
+                        {item.subcategory && <p>
+                                <strong>Subcategory:</strong> {item.subcategory?.name || "N/A"}
+                            </p>}
+                        <p>
+                            <strong>Amount:</strong> <span className={item.isExpense ? "negative" : "positive"}>{item.isExpense ? "-" : "+"} {displayCurrency(item.amount, item.currency.currencyCode)}</span>
+                        </p>
+                    </div>
+                    <div className="transactions-card-footer">
+                        <span>
+                            <strong>Account:</strong> {item.accountName}
+                        </span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default TransactionCards;
