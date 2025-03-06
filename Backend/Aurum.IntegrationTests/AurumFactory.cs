1b6f783fccd78942f1501c0df46891bad45fd8a2
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-06 17:36:16 +0100

    Dataseed test

--- Backend/Aurum.IntegrationTests/AurumFactory.cs
+++ Backend/Aurum.IntegrationTests/AurumFactory.cs
@@ -1,4 +1,5 @@
 using Aurum.Data.Context;
+using Aurum.Data.Entities;
 using Aurum.Services.AccountService;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -14,23 +15,36 @@ public class AurumFactory : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+
+	    builder.UseEnvironment("Testing");
+
         builder.ConfigureServices(services =>
         {
             var aurumDbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AurumContext>));
-            
+
             services.Remove(aurumDbContextDescriptor);
-            
+
             services.AddDbContext<AurumContext>(options =>
             {
                 options.UseInMemoryDatabase(_dbName);
             });
-            
+
             using var scope = services.BuildServiceProvider().CreateScope();
-            
+
             var aurumContext = scope.ServiceProvider.GetRequiredService<AurumContext>();
             aurumContext.Database.EnsureDeleted();
             aurumContext.Database.EnsureCreated();
-            
+
+            var currencies = new List<Currency>()
+            {
+	            new Currency { CurrencyCode = "HUF", Name = "Forint", Symbol = "Ft" },
+	            new Currency { CurrencyCode = "EUR", Name = "Euro", Symbol = "€" },
+	            new Currency { CurrencyCode = "USD", Name = "US Dollar", Symbol = "$" }
+            };
+
+            aurumContext.Currencies.AddRangeAsync(currencies);
+            aurumContext.SaveChangesAsync();
+
             // additional initialization for admin if needed
         });
     }
